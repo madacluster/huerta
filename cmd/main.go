f31365a87f7a8efd983040aa3086e88cc3580203
cesarg <cesarg@empathy.co>
2022-10-07 17:02:58 +0200

    add cmd cli

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	pkg "github.com/madacluster/huerto/pkg"
 	"github.com/spf13/cobra"
 )
 
-var ZERO_HOST = "tcp://192.168.0.110"
+var ZERO_HOST = "tcp://"
 
 func main() {
 	var echoTimes int
@@ -54,8 +55,29 @@ func main() {
 		Args:  cobra.MinimumNArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
 			sensor := pkg.NewGPIOSensor(ZERO_HOST)
-			pin, mode, value, values := sensor.GetData()
-			fmt.Printf("Pin: %d\nMode: %v\nValue: %d\nValues: %d\n", pin, mode, value, values)
+			on, off := sensor.GetData()
+			fmt.Printf("On: %v\nOff: %v\n", on, off)
+		},
+	}
+
+	var cmdReadGPIO = &cobra.Command{
+		Use:   "read",
+		Short: "Read Pin GPIO",
+		Long:  `Read Pin GPIO`,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Println("No pin specified")
+				return
+			} else {
+				pinIn, _ := strconv.Atoi(args[0])
+				sensor := pkg.NewGPIOSensor(ZERO_HOST)
+				if sensor.ReadPin(pinIn) {
+					fmt.Printf("Pin %d is on\n", pinIn)
+				} else {
+					fmt.Printf("Pin %d is off\n", pinIn)
+				}
+			}
 		},
 	}
 
@@ -63,6 +85,7 @@ func main() {
 
 	var rootCmd = &cobra.Command{Use: "matrix"}
 	rootCmd.AddCommand(cmdHumidity, cmdPressure, cmdUV, cmdGPIO)
-	cmdPressure.AddCommand(cmdUV)
+	cmdGPIO.AddCommand(cmdReadGPIO)
+	rootCmd.PersistentFlags().StringVarP(&ZERO_HOST, "host", "H", "tcp://192.168.0.110", "ZeroMQ host")
 	rootCmd.Execute()
 }
